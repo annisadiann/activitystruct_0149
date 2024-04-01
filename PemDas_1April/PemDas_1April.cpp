d7f41c0acd1565c3f57377cbf423c37af45ae46a
annisadiann <annisadian0412@gmail.com>
2024-04-01 17:41:15 +0700

    Add project files.

---
+++ PemDas_1April/PemDas_1April.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    string name;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
